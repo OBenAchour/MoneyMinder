c3d7d207117492b655db7a832764263410ca5505
OBenAchour <oussema.benachour@esprit.tn>
2024-06-17 18:23:10 +0200

    interfacesHomeHomeAdmin

---
+++ src/main/java/Controllers/HomeAdmin.java
@@ -0,0 +1,42 @@
+package Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeAdmin {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    void Gerer_Transaction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GererTransactions.fxml"));
+            Parent newRoot = loader.load();
+            rootPane.getChildren().setAll(newRoot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
