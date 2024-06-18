fbaae03996aedabba23c06e0023ab6f489dad460
OBenAchour <oussema.benachour@esprit.tn>
2024-06-18 23:12:38 +0200

    controllerTransactionType

--- src/main/java/Controllers/GererTransactionsHA.java
+++ src/main/java/Controllers/GererTransactionsHA.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class GererTransactions {
+public class GererTransactionsHA {
 
     @FXML
     private ResourceBundle resources;
@@ -38,7 +38,21 @@ public class GererTransactions {
     @FXML
     void initialize() {
         to_home_admin.setOnAction(event->to_home_admin());
+        to_transaction_type.setOnAction(event->to_transaction_type());
 
+    }
+
+    private void to_transaction_type() {
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/TransactionType.fxml"));
+        try {
+            Parent root=loader.load();
+            Stage stage=(Stage)to_home_admin.getScene().getWindow();
+            Scene scene=new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void to_home_admin() {
