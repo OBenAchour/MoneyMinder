1c821f7bbce99b0ecf6ceaff60e79c98b8e5fc62
meriammoncer <168366218+meriammoncer@users.noreply.github.com>
2024-06-19 11:39:55 +0100

    bouton HomeAdmin

--- src/main/java/Controller/AjouterType.java
+++ src/main/java/Controller/AjouterType.java
@@ -21,12 +21,21 @@ public class AjouterType {
 
     @FXML
     private Button btnretour;
+
+    @FXML
+    private Button btnHomeAd;
+
+    @FXML
+    private Button btnV;
+
     @FXML
     private TextField typeTextField;
 
     @FXML
     void initialize() {
         btnretour.setOnAction(event -> RetourAjout());
+        btnHomeAd.setOnAction(event -> retHomeAdmin());
+        btnV.setOnAction(event -> ValiderAjout());
     }
 
 
@@ -40,6 +49,36 @@ public class AjouterType {
             stage.setScene(scene);
             stage.show();
             System.out.println("Retour avec succès !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void retHomeAdmin() {
+        try {
+            System.out.println("Chargement de l'interface retHomeAdmin...");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/HomeAdmin.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) btnHomeAd.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            System.out.println("Retour avec succès !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void ValiderAjout() {
+        try {
+            System.out.println("Chargement de l'interface modifier Objectif...");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GestionObjectif.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) btnV.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            System.out.println("Validation avec succès !");
         } catch (IOException e) {
             e.printStackTrace();
         }
